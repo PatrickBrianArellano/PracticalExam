b6ff73407f8d26c05d0afb62012bd4628cfb6b41
Patrick Arellano <brianarellano0428@gmail.com>
2020-07-29 04:49:37 +0800

    Commit 4: Modified the index.ejs and index.js to finish the startup page with GET method and Select query

--- practicalexam/routes/index.js
+++ practicalexam/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../lib/db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+    connection.query('SELECT * FROM items ORDER BY id', function (err, rows) {
+        if (err) {
+            req.flash('error', err);
+            res.render('index', { data: '' });
+        } else {
+
+            res.render('index', { data: rows });
+        }
+
+    });
+
 });
 
 module.exports = router;
